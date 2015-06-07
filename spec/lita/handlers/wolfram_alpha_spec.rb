977164523c94df92b30be0e4a1ed72b0d538fbe3
Tristan Chong <tristanchong@gmail.com>
2015-06-06 18:59:05 -0700

    spec: config; route and #query tests

--- spec/lita/handlers/wolfram_alpha_spec.rb
+++ spec/lita/handlers/wolfram_alpha_spec.rb
@@ -1,4 +1,33 @@
 require "spec_helper"
 
 describe Lita::Handlers::WolframAlpha, lita_handler: true do
+
+  before do
+    registry.config.handlers.wolfram_alpha.app_id = ENV["WA_APP_ID"]
+  end
+
+  it { is_expected.to route_command("wa pi").to(:query) }
+
+  describe "#query" do
+
+    it "returns the primary pod's plaintext for a valid query" do
+      send_command "wa pi"
+      expect(replies.count).to eq 1
+      expect(replies.first).to match(/^3\.14159/)
+    end
+
+    it "returns the first non-input, non-empty pod's plaintext for a valid query when no primary pod exists" do
+      send_command "wa U+00F8"
+      expect(replies.count).to eq 1
+      expect(replies.first).to match(/^Latin small letter o with stroke/)
+    end
+
+    it "returns a shrug emoticon for an unrecognized query" do
+      send_command "wa lkasdjflkajelkfa"
+      expect(replies.count).to eq 1
+      expect(replies.first).to match(/_\(/)
+    end
+
+  end
+
 end
