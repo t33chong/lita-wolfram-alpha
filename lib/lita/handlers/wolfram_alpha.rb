c316b0bf50fc1daaa716a6268fef18f9fb420a67
Tristan Chong <tristanchong@gmail.com>
2015-06-07 12:55:35 -0700

    lib: 'Learn more' link & hide_link option, handle multiple primary subpods

--- lib/lita/handlers/wolfram_alpha.rb
+++ lib/lita/handlers/wolfram_alpha.rb
@@ -1,3 +1,4 @@
+require "cgi"
 require "nokogiri"
 
 module Lita
@@ -7,6 +8,7 @@ module Lita
       API_URL = "http://api.wolframalpha.com/v2/query"
 
       config :app_id, type: String, required: true
+      config :hide_link, types: [TrueClass, FalseClass], default: false
 
       route(/^wa\s+(.+)/i, :query, command: true,
                     help: {t("help.query_key") => t("help.query_value")})
@@ -20,11 +22,21 @@ module Lita
           appid: config.app_id
         )
         return if http_response.status != 200
+        link = "Learn more: http://www.wolframalpha.com/input/?i=#{CGI.escape(input)}"
         xml = Nokogiri::XML(http_response.body)
-        result = xml.at_xpath("//pod[@primary='true']//plaintext")
-        return response.reply result.content if result
-        result = xml.at_xpath("//pod[not(@title='Input interpretation')]//plaintext[string-length(text()) > 0]")
-        response.reply result ? result.content : "¯\\_(ツ)_/¯"
+        primary = xml.at_xpath("//pod[@primary='true']//plaintext[string-length(text()) > 0]")
+        if primary
+          response.reply primary.content
+          response.reply link unless config.hide_link
+        else
+          secondary = xml.at_xpath("//pod[not(@id='Input')]//plaintext[string-length(text()) > 0]")
+          if secondary
+            response.reply secondary.content
+            response.reply link unless config.hide_link
+          else
+            response.reply "¯\\_(ツ)_/¯"
+          end
+        end
       end
 
     end
