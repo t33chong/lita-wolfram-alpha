b514ce6c04791a7b524527fefd92d6489b681cdd
Tristan Chong <tristanchong@gmail.com>
2015-06-14 20:32:38 -0700

    spec: Add title to reply

--- spec/lita/handlers/wolfram_alpha_spec.rb
+++ spec/lita/handlers/wolfram_alpha_spec.rb
@@ -10,32 +10,46 @@ describe Lita::Handlers::WolframAlpha, lita_handler: true do
 
   describe "#query" do
 
-    it "returns the primary pod's plaintext" do
+    it "returns the primary pod's title and plaintext" do
       send_command "wa pi"
       expect(replies.count).to eq 2
-      expect(replies.first).to match(/^3\.14159/)
-      expect(replies.last).to match(/^Learn more: http:\/\/www\.wolframalpha\.com\/input\/\?i=pi$/)
+      expect(replies.first).to match(/^Decimal approximation: 3\.14159/)
+      expect(replies.last).to match(/^http:\/\/www\.wolframalpha\.com\/input\/\?i=pi$/)
     end
 
-    it "returns the first subpod containing plaintext when the primary pod has multiple subpods" do
+    it "returns the title and plaintext of the first subpod containing plaintext when the primary pod has multiple subpods, omitting the pod's title when it contains 'Result'" do
       send_command "wa sky chart"
       expect(replies.count).to eq 2
-      expect(replies.first).to match(/^\d+\^h \d+\^m [\d\.]+\^s$/)
-      expect(replies.last).to match(/^Learn more: http:\/\/www\.wolframalpha\.com\/input\/\?i=sky\+chart$/)
+      expect(replies.first).to match(/^Local sidereal time: \d+\^h \d+\^m [\d\.]+\^s$/)
+      expect(replies.last).to match(/^http:\/\/www\.wolframalpha\.com\/input\/\?i=sky\+chart$/)
     end
 
-    it "returns the first non-input, non-empty pod's plaintext when no primary pod exists" do
+    it "returns the first non-input, non-empty pod's title and plaintext when no primary pod exists" do
       send_command "wa U+00F8"
       expect(replies.count).to eq 2
-      expect(replies.first).to match(/^Latin small letter o with stroke/)
-      expect(replies.last).to match(/^Learn more: http:\/\/www\.wolframalpha\.com\/input\/\?i=U%2B00F8$/)
+      expect(replies.first).to match(/^Name: Latin small letter o with stroke/)
+      expect(replies.last).to match(/^http:\/\/www\.wolframalpha\.com\/input\/\?i=U%2B00F8$/)
+    end
+
+    it "returns the concatenated pod and subpod titles when they exist, separating the titles from the answer with a newline when the answer contains multiple lines" do
+      send_command "wa weather forecast for san francisco"
+      expect(replies.count).to eq 2
+      expect(replies.first).to match(/^Weather forecast for San Francisco, California -> Today:\nbetween/)
+      expect(replies.last).to match(/^http:\/\/www\.wolframalpha\.com\/input\/\?i=weather\+forecast\+for\+san\+francisco$/)
+      end
+
+    it "returns only the plaintext when the pod and subpod titles contain 'Result' or do not exist" do
+      send_command "wa warp speed"
+      expect(replies.count).to eq 2
+      expect(replies.first).to match(/^speed/)
+      expect(replies.last).to match(/^http:\/\/www\.wolframalpha\.com\/input\/\?i=warp\+speed$/)
     end
 
     it "does not return a link if hide_link is true" do
       registry.config.handlers.wolfram_alpha.hide_link = true
       send_command "wa pi"
       expect(replies.count).to eq 1
-      expect(replies.first).to match(/^3\.14159/)
+      expect(replies.first).to match(/^Decimal approximation: 3\.14159/)
     end
 
     it "returns a shrug emoticon for an unrecognized query" do
